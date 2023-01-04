2f34c6f8eecd97efa9f4f451dc8ec635181335fe
ericccccccccccccc2 <113203325+ericccccccccccccc2@users.noreply.github.com>
2023-01-04 13:59:45 -0500

    IT WORKS

--- poopy/main.js
+++ poopy/main.js
@@ -1,1 +1,18 @@
 import "./style.css";
+
+let user = "ericx101";
+let tag = "peee";
+let name_level = `https://api.henrikdev.xyz/valorant/v1/account/${user}/${tag}`;
+
+async function test() {
+  try {
+    const temp = await fetch(name_level);
+    const array = await temp.json();
+    console.log(array);
+    document.getElementById("amongus").innerHTML = `<p>${array.data.name}</p>`;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+test();
