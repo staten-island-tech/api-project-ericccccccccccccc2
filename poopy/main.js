b1f18fb3e2d64ff455f367c0e5488b73e8d3b9b0
ericccccccccccccc2 <113203325+ericccccccccccccc2@users.noreply.github.com>
2023-01-05 13:02:32 -0500

    amongus

--- poopy/main.js
+++ poopy/main.js
@@ -1,18 +1,24 @@
 import "./style.css";
 
-let user = "ericx101";
-let tag = "peee";
-let name_level = `https://api.henrikdev.xyz/valorant/v1/account/${user}/${tag}`;
+const button = document.getElementById("btn");
+const in1 = document.getElementById("name");
+const in2 = document.getElementById("tag");
 
-async function test() {
+async function create() {
   try {
+    let user = in1.value;
+    let tag = in2.value;
+    let name_level = `https://api.henrikdev.xyz/valorant/v1/account/${user}/${tag}`;
+    console.log(name_level);
     const temp = await fetch(name_level);
     const array = await temp.json();
     console.log(array);
-    document.getElementById("amongus").innerHTML = `<p>${array.data.name}</p>`;
+    document.getElementById(
+      "amongus"
+    ).innerHTML = `<p>${array.data.account_level}</p>`;
   } catch (error) {
     console.log(error);
   }
 }
 
-test();
+button.addEventListener("click", create);
